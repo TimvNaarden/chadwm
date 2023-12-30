b879a2c37a1abd82bad438989a27ee1b87f8bb70
Tim van Naarden <TimvNaarden@outlook.com>
2023-12-30 18:13:27 +0100

    fixed dbus problem

--- scripts/run.sh
+++ scripts/run.sh
@@ -7,4 +7,4 @@ xset r rate 200 50 &
 picom &
 
 dash ~/.config/chadwm/scripts/bar.sh &
-while type chadwm >/dev/null; do chadwm && continue || break; done
+while type chadwm >/dev/null; do dbus-launch --exit-with-session chadwm && continue || break; done
